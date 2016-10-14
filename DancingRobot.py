e67713b8bb03e3c1f479fd6314ce78f76ab61186
Haan Zilmer <hzilmer@chadwickschool.org>
2016-10-14 11:07:35 -0700

    add Two Step

--- DancingRobot.py
+++ DancingRobot.py
@@ -1,7 +1,23 @@
 from Myro import *
 init("sim")
 def Sprinkler ():
-    for x in range(0,120):
-        turnRight( 3, .1)
-        turnLeft( 2, .1)
+    for x in range(0,40):
+        turnRight( 3, .5)
+        turnLeft( 2, .5)
 Sprinkler()
+def TwoStep ():
+    forward( 3, 1)
+    backward( 3, 1)
+    turnBy(90)
+    forward( 3, 1)
+    backward( 6, 2)
+    forward( 3, 1)
+    turnBy(270)
+    forward( 3, 1)
+    backward( 3, 1)
+    turnBy(90)
+    forward( 3, 1)
+    backward( 6, 2)
+    forward( 3, 1)
+    turnBy(270)
+TwoStep()
